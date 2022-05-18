566b1fa397a8398f62d2ded255391bc8173c055a
Tan Jin Yi <rayneedust@gmail.com>
2022-05-18 17:37:24 +0800

    Pass 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
+    if (min > max) {
+        let tempSum = min;
+        min = max;
+        max = tempSum;
+    }
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// Thinking process
+// 1. Take two input numbers
+// 2. Determine if both integers more than zero
+// 3. Add numbers including and in between the 2 args
+// 4. Asssign variable to final sum
+// 5. Return
