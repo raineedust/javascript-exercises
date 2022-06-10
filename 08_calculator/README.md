38054a24f8ebb82f36f3f3ee3ae40b4a6142fcd2
Tan Jin Yi <rayneedust@gmail.com>
2022-06-10 19:24:36 +0800

    Pass 01

--- 08_calculator/README.md
+++ 08_calculator/README.md
@@ -6,3 +6,9 @@ add, subtract, get the sum, multiply, get the power, and find the factorial
 
 In order to do this please fill out each function with your solution. Make sure to return the value so you can test it in Jest! To see the expected value
 take a look at the spec file that houses the Jest test cases.
+
+## Resources
+
+- [javascript - How to find the sum of an array of numbers - Stack Overflow](https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers)
+- [javascript - Multiply all elements in array - Stack Overflow](https://stackoverflow.com/questions/19175063/multiply-all-elements-in-array)
+- [math - What is the fastest factorial function in JavaScript? - Stack Overflow](https://stackoverflow.com/questions/3959211/what-is-the-fastest-factorial-function-in-javascript)
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+let f = [];
+const factorial = function (n) {
+  if (n == 0 || n == 1) return 1;
+  if (f[n] > 0) return f[n];
+  return (f[n] = factorial(n - 1) * n);
 };
 
 // Do not edit below this line
@@ -29,5 +32,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
